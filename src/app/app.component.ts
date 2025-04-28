4140d162e91aa4ac404643476776aa8ec2db0ac1
ridaDev <kiratterida@gmail.com>
2025-04-28 11:52:28 +0100

    créer recettes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../pages/navbar/navbar.component";
 import { HeaderComponent } from "../pages/navbar/header/header.component";
+import { CategorieComponent } from "./categorie/categorie.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, CategorieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

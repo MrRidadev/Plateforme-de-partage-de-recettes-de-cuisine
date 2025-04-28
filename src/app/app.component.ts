d2831afad52db359c41d844d2abebe3ddbce2d9b
ridaDev <kiratterida@gmail.com>
2025-04-28 11:46:17 +0100

    créer composant header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarComponent } from "../pages/navbar/navbar.component";
+import { HeaderComponent } from "../pages/navbar/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

cdafdad75e4d80eb6cc42ee27091fdefd81d376c
ridaDev <kiratterida@gmail.com>
2025-04-30 11:56:28 +0100

    Créer services RatingService

--- src/pages/recipe-detail/recipe-detail.component.ts
+++ src/pages/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule,NgIf ],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
 export class RecipeDetailComponent {
+  comentaire: string = '';
+  comentaireEnvoye: string = '';
+
+  envoyerComentaire() {
+    this.comentaireEnvoye = this.comentaire;
+    this.comentaire = ''; 
+  }
 
 }
